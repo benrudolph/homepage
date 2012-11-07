6a423ab32ed785918bddefe38b53c02036c980a1
Ben Rudolph <rudolphben@gmail.com>
2012-11-06 22:20:09 -0800

    Small fix to browser check

--- public/js/browser_check.js
+++ public/js/browser_check.js
@@ -1,4 +1,3 @@
 if ($.browser.msie) {
   alert("My homepage probably won't work in IE. For best results use Chrome. Alternatively, view my linkedin here: www.linkedin.com/in/brudolph")
 }
-var me = new Me()
--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  if ($.browser.msie) {
-    alert("My homepage probably won't work in IE. For best results use Chrome. Alternatively, view my linkedin here: www.linkedin.com/in/brudolph")
-  }
   var me = new Me()
   me.render()
 })
