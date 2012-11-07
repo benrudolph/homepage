c1f2f2f971c55c8b43403a3f4bced18ae4366602
Ben Rudolph <rudolphben@gmail.com>
2012-11-06 19:26:27 -0800

    IE alert warning

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  if ($.browser.msie) {
+    alert("My homepage probably won't work in IE. For best results use Chrome. Alternatively, view my linkedin here: www.linkedin.com/in/brudolph")
+  }
   var me = new Me()
   me.render()
 })
