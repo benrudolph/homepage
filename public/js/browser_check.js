aee70bf64c0d583f9560955a6b5fd3c94098a4a4
Ben Rudolph <rudolphben@gmail.com>
2012-11-06 22:16:30 -0800

    Fix for msie check

---
+++ public/js/browser_check.js
@@ -0,0 +1,4 @@
+if ($.browser.msie) {
+  alert("My homepage probably won't work in IE. For best results use Chrome. Alternatively, view my linkedin here: www.linkedin.com/in/brudolph")
+}
+var me = new Me()
